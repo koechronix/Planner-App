efb73b68f8076c832ba34c6cf2a1690293782c2b
koechronix <koechronix@yahoo.com>
2022-10-09 19:57:33 +0300

    Added index and show in categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,18 @@
 class CategoriesController < ApplicationController
+    skip_before_action :authorize
+
+    # GET/categories
+    def index
+        render json: Category.all
+    end
+
+    # GET/categories/:id
+    def show
+        render json: find_category
+    end
+
+    private 
+    def find_category
+        Category.find(params[:id])
+    end
 end
