59b3496d78e5db48164ba2cb89b7dde7e8fe8c34
koechronix <koechronix@yahoo.com>
2022-10-09 20:14:26 +0300

    Created sessions controller and ..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+    # GET/users
+    def show
+        render json: @current_user
+    end
+
+    # PATCH/users
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    private
+    def find_user
+        User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:username, :password, :first_name)
+    end
 end
